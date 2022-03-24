fc740338d60b15bac4c0cbe704e1ce142d043223
naufalricho <naufalricho31@students.amikom.ac.id>
2022-03-24 07:42:38 +0000

    tugas

---
+++ Taxi/Taxi/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ClassTaxi;
+
+namespace tugas
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            // membuat ibjek Taxi
+            Taxi taxi = new Taxi();
+
+            // pengesetan nilai properties
+            taxi.DriverName = "Naufal";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            // pemanggilan method
+            taxi.InfoTaxi();
+            taxi.PickUpPassenger();
+            taxi.PickOffPassenger();
+
+            Console.ReadKey();
+
+        }
+    }
+}
